676ac550452727168c5d1ca43e29f5f3be8727d4
beephaha <bektursynov_b@kbtu.kz>
2023-02-09 11:15:30 +0600

    0

--- tsis3/classes/ex6.py
+++ tsis3/classes/ex6.py
@@ -1,7 +1,15 @@
-import sympy
-class primes:
-    def __init__(self, nums):
-        self.nums = nums
-        return nums
-    def filter(self):
-        self.nums = list(filter(lambda x: for i in range(len(nums)): if simpy.isprime())
+def is_prime(num):
+    if num == 1:
+        return False
+    if num > 1:
+        for j in range(2, int(num/2)+1):
+            if (num % j) == 0:
+                return False
+        return True
+    else:
+        return True
+
+my_list = [1,2,3,4,5,6,7,8,9,10,11]
+
+ura_last_task = filter(lambda x: is_prime(x), my_list)
+print(list(ura_last_task))
